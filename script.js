73e153a340f4b6b1a0eb3b066c98b1a292f7c57b
Shakir Ullah Shakir <116002109+shakir5154@users.noreply.github.com>
2025-03-04 15:00:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Portfolio Loaded!");
+
+    const header = document.querySelector("header");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            header.style.background = "#222";
+        } else {
+            header.style.background = "#333";
+        }
+    });
+});
 
